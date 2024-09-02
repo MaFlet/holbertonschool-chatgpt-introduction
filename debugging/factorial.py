ed61003044ed2fda028201d8a961cc4db3fe1e5f
MaFlet <mfletcher2808@hotmail.com>
2024-09-02 20:33:06 +1000

    factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+"""
+This script calucates factorial number passed a command-line argument
+"""
 import sys
 
 def factorial(n):
+    """"
+    Calculate and print factorial of argument.
+    Parameters:
+    n (int): The number for which the factorial is to be calculated.
+    Returns:
+    int: Factorial of the given number.
+    """
     result = 1
     while n > 1:
         result *= n
+        n -= 1
         return result
     n = factorial(int(sys.argv[1]))
     print('Factorial \n Number')
