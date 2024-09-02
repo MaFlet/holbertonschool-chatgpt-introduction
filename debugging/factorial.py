1d9b038d19674cd293afcf3bb7ac463b666c9f33
MaFlet <mfletcher2808@hotmail.com>
2024-09-02 20:22:17 +1000

    factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,8 +5,6 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1
-        return result 
-    
-    f = factorial(int(sys.argv[1]))
-    print(f)
+        return result
+    n = factorial(int(sys.argv[1]))
+    print('Factorial \n Number')
